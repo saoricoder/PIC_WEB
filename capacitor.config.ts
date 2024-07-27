2af66cb06336717c6ad16bc109247a0a73d6120f
saoricoder <blrodriguez3@espe.edu.ec>
2024-07-27 08:15:32 -0500

    cambio realizados
    :pig:

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'proyect1',
-  webDir: 'www'
-};
-
-export default config;
